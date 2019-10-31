f52aa406bd080b25ca594664e2a87ff351f16da1
mendosaurus <masaki.endo@nex.nikkei.com>
2019-10-31 09:30:32 -0400

    update 6 kyu Unique in Order

--- JavaScript/6kyu/unique in order.js
+++ JavaScript/6kyu/unique in order.js
@@ -3,16 +3,33 @@
 // Implement the function unique_in_order which takes as argument a sequence and returns a list of items without any elements with the same value next to each other and preserving the original order of elements.
 //remember iterable can be a string or an array
 
-let uniqueInOrder = function (iterable) {
-    if (typeof iterable === 'string') {
-        iterable = iterable.split('')
-        console.log(iterable)
+// my solution
+// let uniqueInOrder = function (iterable) {
+//     if (typeof iterable === 'string') {
+//         iterable = iterable.split('')
+//         console.log(iterable)
+//     }
+
+//     return iterable.filter((current, index, arr) => {
+//         return current !== arr[index - 1]
+//     })
+// }
+
+// best practice
+function uniqueInOrder(it) {
+    var result = []
+    var last
+
+    for (var i = 0; i < it.length; i++) {
+        if (it[i] !== last) {
+            last = it[i]
+            result.push(it[i])
+        }
     }
 
-    return iterable.filter((current, index, arr) => {
-        return current !== arr[index - 1]
-    })
+    return result
 }
+
 
 // call function
 uniqueInOrder('AAAABBBCCDAABBB')
