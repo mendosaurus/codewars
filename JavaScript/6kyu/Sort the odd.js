64ebe6da596d1d1a51281db0803a45503a050d27
mendosaurus <masaki.endo@nex.nikkei.com>
2019-11-05 23:42:17 -0500

    complete 6 kyu Sort the odd

---
+++ JavaScript/6kyu/Sort the odd.js
@@ -0,0 +1,47 @@
+// https://www.codewars.com/kata/sort-the-odd/train/javascript
+
+// You have an array of numbers.
+// Your task is to sort ascending odd numbers but even numbers must be on their places.
+
+// Zero isn't an odd number and you don't need to move it. If you have an empty array, you need to return it.
+
+// Example
+// sortArray([5, 3, 2, 8, 1, 4]) == [1, 3, 2, 8, 5, 4]
+
+// my solution
+// function sortArray(array) {
+// 	const odd = [];
+// 	const even = [];
+// 	const result = [];
+
+// 	array.forEach(number => {
+// 		if(number % 2 === 0){
+// 			even.push(number);
+// 		}
+// 		else{
+// 			odd.push(number);
+// 		}
+// 	});
+
+// 	odd.sort((a,b) => a-b); //sort odd array
+// 	console.log(odd, even);
+
+// 	array.forEach(number => {
+// 		if(number % 2 === 0){
+// 			result.push(even.shift());
+// 		}
+// 		else{
+// 			result.push(odd.shift());
+// 		}
+// 	})
+// 	return result;
+// }
+
+
+// best practice
+function sortArray(array) {
+   const odd = array.filter(number => number % 2).sort((a,b) => a-b);
+   return array.map((x) => x % 2 ? odd.shift() : x);
+}
+
+sortArray([5, 333, 2, 8, 1, 4])
