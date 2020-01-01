89e9283f5aee88adca20b782cb230b40649b6e98
mendosaurus <masaki.endo@nex.nikkei.com>
2020-01-01 19:14:42 +0900

    complete 7 kyu Vowel Count

---
+++ JavaScript/7kyu/Vowel Count.js
@@ -0,0 +1,41 @@
+// https://www.codewars.com/kata/vowel-count/train/javascript
+
+// Return the number (count) of vowels in the given string.
+// We will consider a, e, i, o, and u as vowels for this Kata.
+// The input string will only consist of lower case letters and/or spaces.
+
+// function getCount(str) {
+//   var vowelsCount = 0;
+
+//   // go through the string by using for loop
+//   for (let i = 0; i<str.length; i++){
+//     console.log(str[i]);
+//     // if a letter in a string is equal to one of a vewels, add 1 to vowelsCount variable
+//     if (str[i] === 'a' ||str[i] === 'e' ||str[i] === 'i' ||str[i] === 'o' ||str[i] === 'u'){
+//       vowelsCount ++
+//     }
+//   }
+//   return vowelsCount;
+// }
+
+// best practice
+function getCount(str) {
+  var vowelsCount = 0;
+  let vowels = ["a", "e", "i", "o", "u"];
+
+  for (let i = 0; i < str.length; i++) {
+    console.log(str[i]);
+    for (let j = 0; j < vowels.length; j++) {
+      if (str[i] === vowels[j]) {
+        console.log("vowel! add one!");
+        vowelsCount++;
+        console.log(vowelsCount);
+      }
+    }
+  }
+
+  return vowelsCount;
+}
+
+getCount("abracadabraghgbbhgidhfbbf");
+// should return 5
