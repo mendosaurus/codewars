d1f12a253972c2724dfc4df1cb6793840bbbc839
Masaki Endo <masaki.endo@nex.nikkei.com>
2020-01-22 16:24:01 +0900

    6 kyu pokemon damage calculator

--- JavaScript/6kyu/Pokemon Damage Calculator.js
+++ JavaScript/6kyu/Pokemon Damage Calculator.js
@@ -13,6 +13,7 @@
 // Super effective: 2x damage
 // Neutral: 1x damage
 // Not very effective: 0.5x damage
+
 // To prevent this kata from being tedious, you'll only be dealing with four types: fire, water, grass, and electric. Here is the effectiveness of each matchup:
 
 // fire > grass
@@ -29,3 +30,42 @@
 // 2.the opponent's type
 // 3.your attack power
 // 4.the opponent's defense
+
+function calculateDamage(yourType, opponentType, attack, defense) {
+  let effectiveness = {
+    fire: {
+      fire: 0.5,
+      grass: 2,
+      water: 0.5,
+      electric: 1
+    },
+    grass: {
+      fire: 0.5,
+      grass: 0.5,
+      water: 2,
+      electric: 1
+    },
+    water: {
+      fire: 2,
+      grass: 0.5,
+      water: 0.5,
+      electric: 0.5
+    },
+    electric: {
+      fire: 1,
+      grass: 1,
+      water: 2,
+      electric: 0.5
+    }
+  };
+
+  return (damage = 50 * (attack / defense) * effectiveness.attack.defense);
+}
+
+calculateDamage("fire", "water", 100, 100); // => 25
+// calculateDamage("grass", "water", 100, 100); // => 100
+// calculateDamage("electric", "fire", 100, 100); // => 50
+// calculateDamage("grass", "electric", 57, 19); // => 150
+// calculateDamage("grass", "water", 40, 40); // => 100
+// calculateDamage("grass", "fire", 35, 5); // => 175
+// calculateDamage("fire", "electric", 10, 2); // => 250
