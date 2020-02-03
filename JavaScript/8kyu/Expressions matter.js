6139d90c3dd51780443757d19ea59969cbd4991e
Masaki Endo <masaki.endo@nex.nikkei.com>
2020-02-03 14:01:18 +0900

    8 kyu expressions matter

--- JavaScript/8kyu/Expressions matter.js
+++ JavaScript/8kyu/Expressions matter.js
@@ -48,4 +48,16 @@ function expressionMatter(a, b, c) {
   return max;
 }
 
+// best practice
+// function expressionMatter(a, b, c) {
+//   return Math.max(
+//     a + b + c,
+//     a * b * c,
+//     a * (b + c),
+//     (a + b) * c,
+//     a + b * c,
+//     a * b + c
+//   );
+// }
+
 expressionMatter(1, 2, 3);
