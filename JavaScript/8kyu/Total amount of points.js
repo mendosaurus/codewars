122a5dcf7bfe5d93996f6098e997ab8d6b6c3da2
Masaki Endo <masaki.endo@nex.nikkei.com>
2020-02-03 14:53:58 +0900

    8 kyu total amount of points

---
+++ JavaScript/8kyu/Total amount of points.js
@@ -0,0 +1,54 @@
+// https://www.codewars.com/kata/5bb904724c47249b10000131/train/javascript
+// Our football team finished the championship. The result of each match look like "x:y". Results of all matches are recorded in the collection.
+
+// For example: ["3:1", "2:2", "0:1", ...]
+
+// Write a function that takes such collection and counts the points of our team in the championship. Rules for counting points for each match:
+
+// if x>y - 3 points
+// if x<y - 0 point
+// if x=y - 1 point
+// Notes:
+
+// there are 10 matches in the championship
+// 0 <= x <= 4
+// 0 <= y <= 4
+
+function points(games) {
+  let total = 0;
+
+  for (let i = 0; i < games.length; i++) {
+    let eachGame = games[i].split(":");
+    let x = Number(eachGame[0]),
+      y = Number(eachGame[1]);
+    console.log(x, y);
+    if (x > y) {
+      total += 3;
+    } else if (x < y) {
+      total = total;
+    } else {
+      total += 1;
+    }
+  }
+  return total;
+}
+
+points(["1:0", "2:0", "3:0", "4:0", "2:1", "3:1", "4:1", "3:2", "4:2", "4:3"]); // =>30
+
+// describe("Basic tests", function(){
+//   it("Testing for points(['1:0','2:0','3:0','4:0','2:1','3:1','4:1','3:2','4:2','4:3'])", function(){
+//     Test.assertEquals(points(["1:0","2:0","3:0","4:0","2:1","3:1","4:1","3:2","4:2","4:3"]) , 30);
+//   });
+//   it("Testing for points(['1:1','2:2','3:3','4:4','2:2','3:3','4:4','3:3','4:4','4:4'])", function(){
+//     Test.assertEquals(points(["1:1","2:2","3:3","4:4","2:2","3:3","4:4","3:3","4:4","4:4"]) , 10);
+//   });
+//   it("Testing for points(['0:1','0:2','0:3','0:4','1:2','1:3','1:4','2:3','2:4','3:4'])", function(){
+//     Test.assertEquals(points(["0:1","0:2","0:3","0:4","1:2","1:3","1:4","2:3","2:4","3:4"]) , 0);
+//   });
+//   it("Testing for points(['1:0','2:0','3:0','4:0','2:1','1:3','1:4','2:3','2:4','3:4'])", function(){
+//     Test.assertEquals(points(["1:0","2:0","3:0","4:0","2:1","1:3","1:4","2:3","2:4","3:4"]) , 15);
+//   });
+//   it("Testing for points(['1:0','2:0','3:0','4:4','2:2','3:3','1:4','2:3','2:4','3:4'])", function(){
+//     Test.assertEquals(points(["1:0","2:0","3:0","4:4","2:2","3:3","1:4","2:3","2:4","3:4"]) , 12);
+//   });
+// });
