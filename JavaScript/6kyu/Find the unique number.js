ecb3a16c48d5e2af4b2d08314f03e4b9364b5a08
Masaki Endo <masaki.endo@nex.nikkei.com>
2020-02-06 11:23:08 +0900

    6 kyu find the unique number

--- JavaScript/6kyu/Find the unique number.js
+++ JavaScript/6kyu/Find the unique number.js
@@ -15,4 +15,10 @@ function findUniq(arr) {
   return Number(unique);
 }
 
+// best practice
+function findUniq(arr) {
+  arr.sort((a, b) => a - b);
+  return arr[0] == arr[1] ? arr.pop() : arr[0];
+}
+
 findUniq([1, 1, 1, 2, 1, 1]); //=== 2
