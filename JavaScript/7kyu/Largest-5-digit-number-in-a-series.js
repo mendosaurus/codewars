4da366ef732048f9495474bb9a7644a6e0748f9c
mendosaurus <masaki.endo@nex.nikkei.com>
2020-05-31 11:46:13 +0900

    7 kyu Largest 5 digit number in a series

--- JavaScript/7kyu/Largest-5-digit-number-in-a-series.js
+++ JavaScript/7kyu/Largest-5-digit-number-in-a-series.js
@@ -10,3 +10,16 @@
 // 67890 is the greatest sequence of 5 consecutive digits.
 
 // Complete the solution so that it returns the greatest sequence of five consecutive digits found within the number given. The number will be passed in as a string of only digits. It should return a five digit integer. The number passed may be as large as 1000 digits.
+
+function solution(digits) {
+  let answer = 0;
+
+  for (let i = 0; i < digits.length; i++) {
+    let number = digits.substring(i, i + 5);
+
+    if (Number(number) > answer) {
+      answer = Number(number);
+    }
+  }
+  return answer;
+}
