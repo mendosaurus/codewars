3d99ef245882b8fd6cac6c63639d75230e6c12cb
mendosaurus <masaki.endo@nex.nikkei.com>
2020-06-05 17:11:31 +0900

    7 kyu The highest profit wins!

--- JavaScript/7kyu/The highest profit wins!.js
+++ JavaScript/7kyu/The highest profit wins!.js
@@ -15,23 +15,23 @@ function minMax(arr) {
   return [0, 0]; // fix me!
 }
 
-var test = function (arr, res) {
-  Test.assertSimilar(
-    minMax(arr),
-    res,
-    "tested on " + JSON.stringify(arr) + ":"
-  );
-};
+// var test = function (arr, res) {
+//   Test.assertSimilar(
+//     minMax(arr),
+//     res,
+//     "tested on " + JSON.stringify(arr) + ":"
+//   );
+// };
 
-Test.describe("minMax", function () {
-  Test.it("should work for some examples", function () {
-    var i, r;
-    test([1, 2, 3, 4, 5], [1, 5]);
-    test([2334454, 5], [5, 2334454]);
+// Test.describe("minMax", function () {
+//   Test.it("should work for some examples", function () {
+//     var i, r;
+//     test([1, 2, 3, 4, 5], [1, 5]);
+//     test([2334454, 5], [5, 2334454]);
 
-    for (i = 0; i < 20; ++i) {
-      r = Math.random();
-      test([r], [r, r]);
-    }
-  });
-});
+//     for (i = 0; i < 20; ++i) {
+//       r = Math.random();
+//       test([r], [r, r]);
+//     }
+//   });
+// });
