dc47670c8b8ada278a6b8b8ccaee71aa0ebe681a
mendosaurus <masaki.endo@nex.nikkei.com>
2020-06-06 17:25:54 +0900

    7 kyu The highest profit wins!

--- JavaScript/7kyu/The highest profit wins!.js
+++ JavaScript/7kyu/The highest profit wins!.js
@@ -12,7 +12,9 @@
 // All arrays or lists will always have at least one element, so you don't need to check the length. Also, your function will always get an array or a list, you don't have to check for null, undefined or similar.
 
 function minMax(arr) {
-  return [0, 0]; // fix me!
+  let min = Math.min(...arr);
+  let max = Math.max(...arr);
+  return (result = [min, max]);
 }
 
 // var test = function (arr, res) {
