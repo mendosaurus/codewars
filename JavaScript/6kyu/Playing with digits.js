b1575faded4b27b8011e5c972a85550f9b3b55b4
mendosaurus <masaki.endo@nex.nikkei.com>
2020-07-13 10:51:32 +0900

    4 kyu playing with digits

--- JavaScript/6kyu/Playing with digits.js
+++ JavaScript/6kyu/Playing with digits.js
@@ -22,3 +22,21 @@
 // digPow(92, 1) should return -1 since there is no k such as 9¹ + 2² equals 92 * k
 // digPow(695, 2) should return 2 since 6² + 9³ + 5⁴= 1390 = 695 * 2
 // digPow(46288, 3) should return 51 since 4³ + 6⁴+ 2⁵ + 8⁶ + 8⁷ = 2360688 = 46288 * 51
+
+function digPow(n, p) {
+  let string = n.toString();
+  let len = string.length;
+  let result = 0;
+  for (var i = 0; i < len; i++) {
+    var numberser = parseInt(string.charAt(i), 10);
+    result += Math.pow(numberser, p + i);
+  }
+  var x = Math.pow(n, p);
+  if (result === x) {
+    return p;
+  } else if (result % n === 0) {
+    return result / n;
+  } else {
+    return -1;
+  }
+}
