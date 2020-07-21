dac6ece33fb561612a998e641ca91be1cd13ce37
mendosaurus <masaki.endo@nex.nikkei.com>
2020-07-21 09:37:26 +0900

    6 kyu Title Case

--- JavaScript/6kyu/Title Case.js
+++ JavaScript/6kyu/Title Case.js
@@ -17,3 +17,5 @@
 // titleCase('a clash of KINGS', 'a an the of') // should return: 'A Clash of Kings'
 // titleCase('THE WIND IN THE WILLOWS', 'The In') // should return: 'The Wind in the Willows'
 // titleCase('the quick brown fox') // should return: 'The Quick Brown Fox'
+
+function titleCase(title, minorWords) {}
