f6576556327be7801a2edc1d0bab3226e3f18a6c
mendosaurus <masaki.endo@nex.nikkei.com>
2021-01-31 11:13:04 +0900

    first solution

--- JavaScript/8kyu/Difference of Volumes of Cuboids.js
+++ JavaScript/8kyu/Difference of Volumes of Cuboids.js
@@ -7,3 +7,21 @@
 // Your function will be tested with pre-made examples as well as random ones.
 
 // If you can, try writing it in one line of code.
+
+function findDifference(a, b) {
+  const a_volume = a[0] * a[1] * a[2];
+  const b_volume = b[0] * b[1] * b[2];
+  const difference = Math.abs(a_volume - b_volume);
+  return difference;
+}
+
+// Sample tests:
+// describe("Tests", function () {
+//   it("Basic tests", function () {
+//     Test.assertEquals(findDifference([3, 2, 5], [1, 4, 4]), 14);
+//     Test.assertEquals(findDifference([9, 7, 2], [5, 2, 2]), 106);
+//     Test.assertEquals(findDifference([11, 2, 5], [1, 10, 8]), 30);
+//     Test.assertEquals(findDifference([4, 4, 7], [3, 9, 3]), 31);
+//     Test.assertEquals(findDifference([15, 20, 25], [10, 30, 25]), 0);
+//   });
+// });
