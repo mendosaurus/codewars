2b403597c99393bef74454577da40f79962483cd
mendosaurus <masaki.endo@nex.nikkei.com>
2021-02-07 19:53:00 +0900

    8 kyu grasshopper.js

--- JavaScript/8kyu/Grasshopper - Grade book.js
+++ JavaScript/8kyu/Grasshopper - Grade book.js
@@ -10,6 +10,17 @@
 
 function getGrade(s1, s2, s3) {
   // Code here
+  const average = (s1 + s2 + s3) / 3;
+
+  if (average < 60) {
+    return "F";
+  } else if (average < 70) {
+    return "D";
+  } else if (average < 80) {
+    return "C";
+  } else if (average < 90) {
+    return "B";
+  } else return "A";
 }
 
 // // sample test
