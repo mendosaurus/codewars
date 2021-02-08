00e643775fee0589123134828e5e90b88cda3f0d
mendosaurus <masaki.endo@nex.nikkei.com>
2021-02-08 11:39:45 +0900

    6 kyu backspaces in string

---
+++ JavaScript/6kyu/Backspaces in string.js
@@ -0,0 +1,18 @@
+// https://www.codewars.com/kata/5727bb0fe81185ae62000ae3/train/javascript
+
+// Assume "#" is like a backspace in string. This means that string "a#bc#d" actually is "bd"
+
+// Your task is to process a string with "#" symbols.
+
+// Examples
+// "abc#d##c"      ==>  "ac"
+// "abc##d######"  ==>  ""
+// "#######"       ==>  ""
+// ""              ==>  ""
+
+function cleanString(s) {
+  // ... your code ...
+}
+
+// Test.assertEquals(cleanString("abc#d##c"), "ac");
+// Test.assertEquals(cleanString("abc####d##c#"), "");
