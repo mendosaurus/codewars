80d9a93cf113fdc41679ef05ace97cdbaab659ad
mendosaurus <masaki.endo@nex.nikkei.com>
2021-02-14 17:18:12 +0900

    7kyu Mumbling

--- JavaScript/7kyu/Mumbling.js
+++ JavaScript/7kyu/Mumbling.js
@@ -8,15 +8,23 @@
 // accum("cwAt") -> "C-Ww-Aaa-Tttt"
 // The parameter of accum is a string which includes only letters from a..z and A..Z.
 
-function accum(s) {
-  let letters = s.toLowerCase().split("");
-  console.log(letters);
-  for (let i = 0; i < letters.length; i++) {
-    console.log(letters[i]);
-    letters[i] = letters[i].toUpperCase() + letters[i].repeat(i);
-    console.log(letters[i]);
-  }
-  return letters.join("-");
-}
+// function accum(s) {
+//   let letters = s.toLowerCase().split("");
+//   console.log(letters);
+//   for (let i = 0; i < letters.length; i++) {
+//     console.log(letters[i]);
+//     letters[i] = letters[i].toUpperCase() + letters[i].repeat(i);
+//     console.log(letters[i]);
+//   }
+//   return letters.join("-");
+// }
 
-accum("eMlet");
+// accum("eMlet");
+
+function accum(s) {
+  const lowerCase = s.toLowerCase().split("");
+  for (i in lowerCase) {
+    lowerCase[i] = lowerCase[i].toUpperCase() + lowerCase[i].repeat(i);
+  }
+  return lowerCase.join("-");
+}
